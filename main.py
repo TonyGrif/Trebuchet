81525ba86968ef58be71d4e3d485e0d3dcc2839f
TonyGrif <TonyGriffin2000@gmail.com>
2023-12-16 15:39:42 -0500

    add main driver

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""The main module for the trebuchet program.
+"""
+
+def main():
+    """The main driver for the trebuchet program.
+    """
+    print("Hello World!")
+    return
+
+
+if __name__ == "__main__":
+    main()
