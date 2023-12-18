d8e9d71ca27209b26240200308b1841da1fbfaab
TonyGrif <TonyGriffin2000@gmail.com>
2023-12-18 14:47:41 -0500

    replace first index of a word with number

--- src/parse.py
+++ src/parse.py
@@ -8,11 +8,11 @@ from enum import Enum
 
 
 class Alias(Enum):
-    """This enum contains single digit number/word pairs.
-    """
+    """This enum contains single digit number/word pairs."""
+
     zero = 0
     one = 1
-    two = 2 
+    two = 2
     three = 3
     four = 4
     five = 5
@@ -20,6 +20,7 @@ class Alias(Enum):
     seven = 7
     eight = 8
     nine = 9
+
 
 def parse_file(file: TextIO) -> Iterator[Tuple[str, int]]:
     """Parse the provided file for valid lines.
@@ -32,14 +33,18 @@ def parse_file(file: TextIO) -> Iterator[Tuple[str, int]]:
         by concatenating the first and last digit.
     """
     for line in file:
+        if line is None:
+            yield 0
+
+        logging.debug("Parsing %s", line.strip())
         digits = parse_line(line.strip())
         logging.debug("%s parsed from %s", digits, line.strip())
 
         try:
             result = int(str(digits[0]) + str(digits[-1]))
         except IndexError as e:
-            logging.debug("%s produced %e", line.strip, e)
-            yield None
+            logging.debug("Returning None")
+            yield 0
 
         logging.debug("%s returned", result)
         yield (line.strip(), result)
@@ -54,21 +59,31 @@ def parse_line(line: str) -> List[int]:
     Returns:
         A list containing all integers found.
     """
-    _word_to_digit(line)
+    line = _word_to_digit(line)
     return [int(num) for num in re.findall(r"\d", line)]
 
 
-def _word_to_digit(line:str) -> str:
+def _word_to_digit(line: str) -> str:
     """Convert words in a string to digits.
 
     Parameters:
         line(str): the string to edit.
 
     Returns:
-        An update string with digits.
+        An updated string with the digits
+        at the first index of the word.
     """
-    for numbers in Alias:
-        if re.search(str(numbers.name), line):
-            logging.debug("%s found at ", numbers.name)
+    update_line = list(line)
 
-    return 0
+    for numbers in Alias:
+        rgx = re.search(str(numbers.name), line)
+        if rgx is None:
+            continue
+
+        update_line.insert(rgx.span()[0], str(numbers.value))
+        update_line = (
+            update_line[: rgx.span()[0] + 1] + update_line[rgx.span()[0] + 2 :]
+        )
+        logging.debug("Updated String: %s", "".join(update_line))
+
+    return "".join(update_line)
