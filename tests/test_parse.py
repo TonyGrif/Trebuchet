cbf6bec61cc2b6b6b64adfb15b325322d6fc5146
TonyGrif <TonyGriffin2000@gmail.com>
2023-12-18 09:33:36 -0500

    handle empty list case

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -9,13 +9,22 @@ from parse import parse_file, parse_line
 def file():
     return Path("./resources/puzzle_input.txt")
 
+@pytest.fixture
+def words_file():
+    return Path("./resources/part_two_example.txt")
+
 
 class TestParse:
-    def test_file_parse(self, file):
+    def test_file_parse(self, file, words_file):
         with open(file, "r", encoding="utf-8") as f:
             line = parse_file(f)
             assert next(line) == ("shrzvdcghblt21", 21)
             assert next(line) == ("sixdddkcqjdnzzrgfourxjtwosevenhg9", 99)
+
+        with open(words_file, "r", encoding="utf-8") as f:
+            line = parse_file(f)
+            next(line)
+            assert next(line) is None
 
     def test_line_parse(self):
         assert parse_line("1abc2") == [1, 2]
@@ -24,3 +33,4 @@ class TestParse:
         assert parse_line("treb7uchet") == [7]
 
         assert parse_line("abc123") == [1, 2, 3]
+        assert parse_line("trebuchet") == []
