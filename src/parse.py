619e84b6919eb7fb4d623a4cd80a8c284570a071
TonyGrif <TonyGriffin2000@gmail.com>
2023-12-18 10:55:00 -0500

    find numbers as words

--- src/parse.py
+++ src/parse.py
@@ -4,7 +4,22 @@
 import logging
 import re
 from typing import Iterator, List, TextIO, Tuple
+from enum import Enum
 
+
+class Alias(Enum):
+    """This enum contains single digit number/word pairs.
+    """
+    zero = 0
+    one = 1
+    two = 2 
+    three = 3
+    four = 4
+    five = 5
+    six = 6
+    seven = 7
+    eight = 8
+    nine = 9
 
 def parse_file(file: TextIO) -> Iterator[Tuple[str, int]]:
     """Parse the provided file for valid lines.
@@ -39,4 +54,21 @@ def parse_line(line: str) -> List[int]:
     Returns:
         A list containing all integers found.
     """
+    _word_to_digit(line)
     return [int(num) for num in re.findall(r"\d", line)]
+
+
+def _word_to_digit(line:str) -> str:
+    """Convert words in a string to digits.
+
+    Parameters:
+        line(str): the string to edit.
+
+    Returns:
+        An update string with digits.
+    """
+    for numbers in Alias:
+        if re.search(str(numbers.name), line):
+            logging.debug("%s found at ", numbers.name)
+
+    return 0
