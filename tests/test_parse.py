52927282211f33f8b4d4acfde3f4672861aa24d9
TonyGrif <TonyGriffin2000@gmail.com>
2023-12-19 14:07:22 -0500

    add test case for exception

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -9,13 +9,22 @@ from parse import parse_file, parse_line
 def file():
     return Path("./resources/puzzle_input.txt")
 
+
+@pytest.fixture
+def exception_file():
+    return Path("./tests/resources/exception_tester.txt")
+
+
 class TestParse:
-    def test_file_parse(self, file):
+    def test_file_parse(self, file, exception_file):
         with open(file, "r", encoding="utf-8") as f:
             line = parse_file(f)
             assert next(line) == ("shrzvdcghblt21", 21)
             assert next(line) == ("sixdddkcqjdnzzrgfourxjtwosevenhg9", 69)
 
+        with open(exception_file, "r", encoding="utf-8") as f:
+            line = parse_file(f)
+            assert next(line) is None
 
     def test_line_parse(self):
         assert parse_line("1abc2") == [1, 2]
